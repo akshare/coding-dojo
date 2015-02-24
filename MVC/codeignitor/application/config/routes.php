67dbb52ccfcf497a7dc1b773317bfe5a09c09cb5
Ahmed Khan <a_khan69@yahoo.com>
2015-02-23 21:07:49 -0800

    deleted extra uncompressed codeignitor assignment

--- MVC/codeignitor/application/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$route['default_controller'] = "users";
-$route['welcome'] = "users/welcome";
-$route['signout'] = "users/signout";
-$route['404_override'] = '';
-
-//end of routes.php
--- MVC/codeignitor/application/views/header.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<head>
-		<title><?= $page_title ?></title>
-		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-		<script src="assets/jquery/jquery.js" type="text/javascript"></script>
-	</head>
-	<body>
